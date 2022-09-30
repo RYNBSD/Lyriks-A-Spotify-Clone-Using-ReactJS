be788557738c4d4894cd2728cefd776449292983
RYNBSD <112697887+RYNBSD@users.noreply.github.com>
2022-09-30 15:56:10 +0200

    Update keys.js

--- src/redux/keys.js
+++ src/redux/keys.js
@@ -1,2 +1,2 @@
-export const APIkey = 'e19b8159eemsh0ba6fc791a344e6p1d7f26jsn30f7ca524dea';
-export const geokey = 'at_G4ilv9p9KwSEx5c55pzheaYnEXQtp';
+export const APIkey = '';
+export const geokey = '';
